764c1710c12d905aa75f98a4efed3e4747b44ded
Kent Tokunaga <kenttokunaga@gmail.com>
2018-08-27 12:09:41 -0400

    Add findById, addItem, findAndToggleChecked, findAndUpdateName, findAndDelete to store

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,3 +1,5 @@
+/* global cuid */
+
 const Item = (function() {
   const validateName = function(name) {
     if (!name) {
--- scripts/store.js
+++ scripts/store.js
@@ -1,9 +1,9 @@
-/* global shoppingList, cuid */
+/* global cuid Item */
 
 'use strict';
 
 const store = (function () {
-  const items = [
+  let items = [
     { id: cuid(), name: 'apples', checked: false },
     { id: cuid(), name: 'oranges', checked: false },
     { id: cuid(), name: 'milk', checked: true },
@@ -12,9 +12,48 @@ const store = (function () {
   let hideCheckedItems = false;
   let searchTerm = '';
 
+  const findById = function(id) {
+    return this.items.find(function(item) {
+      return item.id === id;
+    });
+  };
+
+  const addItem = function(name) {
+    try {
+      Item.validateName(name);
+      this.items.push(Item.create(name));
+    } catch(e) {
+      console.log(e);
+    }
+  };
+
+  const findAndToggleChecked = function(id) {
+    this.findById(id).checked = !this.findById(id).checked;
+  };
+
+  const findAndUpdateName = function(id, newName) {
+    try {
+      Item.validateName(newName);
+      this.findById(id).name = newName;
+    } catch(e) {
+      console.log(e);
+    }
+  };
+
+  const findAndDelete = function(id) {
+    this.items = this.items.filter(function(item) {
+      return item.id !== id;
+    });
+  };
+
   return {
     items,
     hideCheckedItems,
-    searchTerm
-  }
+    searchTerm,
+    findById, 
+    addItem,
+    findAndToggleChecked,
+    findAndUpdateName,
+    findAndDelete
+  };
 }() );
