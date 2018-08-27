fd76b2e761ec7ff9536e60ac5016f95801a9ac0f
Kent Tokunaga <kenttokunaga@gmail.com>
2018-08-27 11:16:31 -0400

    move store to store.js

---
+++ scripts/store.js
@@ -0,0 +1,21 @@
+/* global shoppingList, cuid */
+
+'use strict';
+
+const store = (function () {
+  const foo = 'bar';
+  const items = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  let hideCheckedItems = false;
+  let searchTerm = '';
+
+  return {
+    items,
+    hideCheckedItems,
+    searchTerm
+  }
+}() );
