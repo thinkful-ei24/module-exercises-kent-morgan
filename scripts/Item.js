c8b8747daf8e6d41fbff900caaf17582aa426b54
Kent Tokunaga <kenttokunaga@gmail.com>
2018-08-27 11:37:53 -0400

    add validateName and create to Item

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,3 +1,19 @@
 const Item = (function() {
-  return {};
+  const validateName = function(name) {
+    if (!name) {
+      throw new ReferenceError('Name does not exist.');
+    }
+  };
+
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false
+    }
+  };
+  return {
+    validateName,
+    create
+  };
 }());
